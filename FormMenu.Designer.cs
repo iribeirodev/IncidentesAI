922e79c73f8acaa499db036db5a7005b5f5bdd74
Itamar Ribeiro <itamar.junior@br.abb.com>
2026-03-05 10:53:40 -0300

    feat: Alterando posicionamento dos botões

--- FormMenu.Designer.cs
+++ FormMenu.Designer.cs
@@ -30,14 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMenu));
             pnlPrincipal = new Panel();
+            Panel03 = new Panel();
+            Titulo03 = new Label();
+            Configuracoes = new PictureBox();
             panel4 = new Panel();
             lblDescricao = new Label();
             Panel02 = new Panel();
             Titulo02 = new Label();
             Prompt = new PictureBox();
-            Panel03 = new Panel();
-            Titulo03 = new Label();
-            Configuracoes = new PictureBox();
             Panel01 = new Panel();
             ListaIncidentes = new PictureBox();
             Titulo01 = new Label();
@@ -45,11 +45,11 @@
             Sair = new PictureBox();
             lblTitulo = new Label();
             pnlPrincipal.SuspendLayout();
+            Panel03.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)Configuracoes).BeginInit();
             panel4.SuspendLayout();
             Panel02.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)Prompt).BeginInit();
-            Panel03.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)Configuracoes).BeginInit();
             Panel01.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)ListaIncidentes).BeginInit();
             pnlTitulo.SuspendLayout();
@@ -59,14 +59,54 @@
             // pnlPrincipal
             // 
             pnlPrincipal.Anchor = AnchorStyles.None;
+            pnlPrincipal.Controls.Add(Panel03);
             pnlPrincipal.Controls.Add(panel4);
             pnlPrincipal.Controls.Add(Panel02);
-            pnlPrincipal.Controls.Add(Panel03);
             pnlPrincipal.Controls.Add(Panel01);
             pnlPrincipal.Location = new Point(81, 79);
             pnlPrincipal.Name = "pnlPrincipal";
             pnlPrincipal.Size = new Size(1001, 610);
             pnlPrincipal.TabIndex = 19;
+            // 
+            // Panel03
+            // 
+            Panel03.BackColor = Color.FromArgb(30, 30, 30);
+            Panel03.Controls.Add(Titulo03);
+            Panel03.Controls.Add(Configuracoes);
+            Panel03.Cursor = Cursors.Hand;
+            Panel03.Location = new Point(122, 27);
+            Panel03.Name = "Panel03";
+            Panel03.Size = new Size(756, 130);
+            Panel03.TabIndex = 22;
+            Panel03.Tag = "Database e Importação ServiceNow";
+            Panel03.Click += ExibirFormulario;
+            // 
+            // Titulo03
+            // 
+            Titulo03.AutoSize = true;
+            Titulo03.BackColor = Color.Transparent;
+            Titulo03.Font = new Font("Segoe UI Variable Display", 20.25F, FontStyle.Bold);
+            Titulo03.ForeColor = Color.FromArgb(224, 224, 224);
+            Titulo03.Location = new Point(291, 46);
+            Titulo03.Name = "Titulo03";
+            Titulo03.Size = new Size(200, 36);
+            Titulo03.TabIndex = 13;
+            Titulo03.Text = "Configurações";
+            Titulo03.Click += ExibirFormulario;
+            // 
+            // Configuracoes
+            // 
+            Configuracoes.BackColor = Color.Transparent;
+            Configuracoes.Cursor = Cursors.Hand;
+            Configuracoes.Image = (Image)resources.GetObject("Configuracoes.Image");
+            Configuracoes.Location = new Point(48, 3);
+            Configuracoes.Margin = new Padding(5);
+            Configuracoes.Name = "Configuracoes";
+            Configuracoes.Size = new Size(158, 122);
+            Configuracoes.SizeMode = PictureBoxSizeMode.CenterImage;
+            Configuracoes.TabIndex = 12;
+            Configuracoes.TabStop = false;
+            Configuracoes.Tag = "Configurações Gerais";
             // 
             // panel4
             // 
@@ -92,7 +132,7 @@
             Panel02.Controls.Add(Titulo02);
             Panel02.Controls.Add(Prompt);
             Panel02.Cursor = Cursors.Hand;
-            Panel02.Location = new Point(122, 171);
+            Panel02.Location = new Point(122, 165);
             Panel02.Name = "Panel02";
             Panel02.Size = new Size(756, 130);
             Panel02.TabIndex = 20;
@@ -126,53 +166,13 @@
             Prompt.TabStop = false;
             Prompt.Tag = "Configuração do Prompt (Persona)";
             // 
-            // Panel03
-            // 
-            Panel03.BackColor = Color.FromArgb(30, 30, 30);
-            Panel03.Controls.Add(Titulo03);
-            Panel03.Controls.Add(Configuracoes);
-            Panel03.Cursor = Cursors.Hand;
-            Panel03.Location = new Point(122, 307);
-            Panel03.Name = "Panel03";
-            Panel03.Size = new Size(756, 130);
-            Panel03.TabIndex = 19;
-            Panel03.Tag = "Database e Importação ServiceNow";
-            Panel03.Click += ExibirFormulario;
-            // 
-            // Titulo03
-            // 
-            Titulo03.AutoSize = true;
-            Titulo03.BackColor = Color.Transparent;
-            Titulo03.Font = new Font("Segoe UI Variable Display", 20.25F, FontStyle.Bold);
-            Titulo03.ForeColor = Color.FromArgb(224, 224, 224);
-            Titulo03.Location = new Point(291, 46);
-            Titulo03.Name = "Titulo03";
-            Titulo03.Size = new Size(200, 36);
-            Titulo03.TabIndex = 13;
-            Titulo03.Text = "Configurações";
-            Titulo03.Click += ExibirFormulario;
-            // 
-            // Configuracoes
-            // 
-            Configuracoes.BackColor = Color.Transparent;
-            Configuracoes.Cursor = Cursors.Hand;
-            Configuracoes.Image = (Image)resources.GetObject("Configuracoes.Image");
-            Configuracoes.Location = new Point(48, 3);
-            Configuracoes.Margin = new Padding(5);
-            Configuracoes.Name = "Configuracoes";
-            Configuracoes.Size = new Size(158, 122);
-            Configuracoes.SizeMode = PictureBoxSizeMode.CenterImage;
-            Configuracoes.TabIndex = 12;
-            Configuracoes.TabStop = false;
-            Configuracoes.Tag = "Configurações Gerais";
-            // 
             // Panel01
             // 
             Panel01.BackColor = Color.FromArgb(30, 30, 30);
             Panel01.Controls.Add(ListaIncidentes);
             Panel01.Controls.Add(Titulo01);
             Panel01.Cursor = Cursors.Hand;
-            Panel01.Location = new Point(122, 35);
+            Panel01.Location = new Point(122, 303);
             Panel01.Name = "Panel01";
             Panel01.Size = new Size(756, 130);
             Panel01.TabIndex = 18;
@@ -258,13 +258,13 @@
             WindowState = FormWindowState.Maximized;
             Paint += FormMenu_Paint;
             pnlPrincipal.ResumeLayout(false);
+            Panel03.ResumeLayout(false);
+            Panel03.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)Configuracoes).EndInit();
             panel4.ResumeLayout(false);
             Panel02.ResumeLayout(false);
             Panel02.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)Prompt).EndInit();
-            Panel03.ResumeLayout(false);
-            Panel03.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)Configuracoes).EndInit();
             Panel01.ResumeLayout(false);
             Panel01.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)ListaIncidentes).EndInit();
@@ -281,14 +281,14 @@
         private Panel Panel02;
         private Label Titulo02;
         private PictureBox Prompt;
-        private Panel Panel03;
-        private Label Titulo03;
-        private PictureBox Configuracoes;
         private Panel Panel01;
         private Label Titulo01;
         private PictureBox ListaIncidentes;
         private Panel pnlTitulo;
         private PictureBox Sair;
         private Label lblTitulo;
+        private Panel Panel03;
+        private Label Titulo03;
+        private PictureBox Configuracoes;
     }
 }
